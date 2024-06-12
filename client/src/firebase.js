07a9740b83721a6878b04069e839202a1f5b1bd2
Chayan Rastogi <cp.chayanrastogi@gmail.com>
2024-06-12 21:41:37 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaf5CnGbOI9UJnX-jq2HkoSTCu54JIMKk",
+  authDomain: "mern-blog-7af64.firebaseapp.com",
+  projectId: "mern-blog-7af64",
+  storageBucket: "mern-blog-7af64.appspot.com",
+  messagingSenderId: "272248770790",
+  appId: "1:272248770790:web:f7b727b8449a85d2aeab27"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto items-center'>
+        <h1 className='text-3xl font-bold lg:text-6xl w-3/5'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm w-3/5'>
+          Here you'll find a variety of articles on trending topics.
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline w-3/5'
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="hr w-full flex justify-center">
+        <hr className='border-t-2 border-[#0694a2] w-4/5 ' />
+      </div>
+
+      {/* <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div> */}
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7 '>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4 justify-evenly'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
